aae9a97e04fa51be72536f04383c13d4a659b792
Li-Heng Hsu <yesleon@me.com>
2018-01-29 01:03:50 +0000

    Initial Commit

---
+++ Closurized/Closurized.h
@@ -0,0 +1,19 @@
+//
+//  Closurized.h
+//  Closurized
+//
+//  Created by Li-Heng Hsu on 29/01/2018.
+//  Copyright © 2018 Li-Heng Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Closurized.
+FOUNDATION_EXPORT double ClosurizedVersionNumber;
+
+//! Project version string for Closurized.
+FOUNDATION_EXPORT const unsigned char ClosurizedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Closurized/PublicHeader.h>
+
+
