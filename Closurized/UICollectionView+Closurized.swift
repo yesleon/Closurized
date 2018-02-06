a40d032bc15124e250cc802e73e0498e2cf1687a
Li-Heng Hsu <yesleon@me.com>
2018-02-06 06:58:45 +0000

    save

---
+++ Closurized/UICollectionView+Closurized.swift
@@ -0,0 +1,59 @@
+//
+//  UICollectionView+Closurized.swift
+//  Closurized
+//
+//  Created by Li-Heng Hsu on 06/02/2018.
+//  Copyright © 2018 Li-Heng Hsu. All rights reserved.
+//
+
+import Foundation
+
+extension UICollectionView: Closurized {
+    
+    class ClosureWrapper: NSObject, UITableViewDataSource, UITableViewDelegate {
+        var numberOfRowsInSection: ((Int) -> Int)!
+        var cellForRowAt: ((IndexPath) -> UITableViewCell)!
+        var didSelectRowAt: ((IndexPath) -> Void)?
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return numberOfRowsInSection(section)
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            return cellForRowAt(indexPath)
+        }
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            didSelectRowAt?(indexPath)
+        }
+        
+    }
+    
+    func makeClosureWrapper() -> ClosureWrapper {
+        return ClosureWrapper()
+    }
+    
+    public enum HandlerEnum {
+        case numberOfRowsInSection((Int) -> Int)
+        case cellForRowAt((IndexPath) -> UITableViewCell)
+        case didSelectRowAt(((IndexPath) -> Void)?)
+    }
+    
+//    public func setHandler(_ handler: HandlerEnum) {
+//        if delegate !== closureWrapper {
+//            delegate = closureWrapper
+//        }
+//        if dataSource !== closureWrapper {
+//            dataSource = closureWrapper
+//        }
+//        switch handler {
+//        case .numberOfRowsInSection(let handler):
+//            closureWrapper.numberOfRowsInSection = handler
+//        case .cellForRowAt(let handler):
+//            closureWrapper.cellForRowAt = handler
+//        case .didSelectRowAt(let handler):
+//            closureWrapper.didSelectRowAt = handler
+//        }
+//    }
+    
+}
